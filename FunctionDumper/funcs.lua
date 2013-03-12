850a28511735b57f75b4bb4d68c061ba2dd110cc
ILOVEPIE <thehairyrock@gmail.com>
2013-03-12 12:07:12 -0700

    Redone function dumping, planning on doing documentation by hand for right now... later on will use web-api for getting documentation.
    Signed-off-by: ILOVEPIE <thehairyrock@gmail.com>

--- FunctionDumper/funcs.lua
+++ FunctionDumper/funcs.lua
@@ -1,9 +1,9 @@
-out = {"{"} -- hueg json table
+out = {"<?xml version=\"1.0\"?>\n<FunctionList>"} -- huge xml table
 for k, v in pairs(wire_expression2_funcs) do
     if string.sub(v[1], 1, 3) ~= "op:" then -- don't include operators
         id, args = string.match(v[1], "(%w+)%(([^%)]*)%)") -- pull identifier and arguments out of signature
-        table.insert(out, ([[["Name": "%q","Arguments":"%q","Return":"%q"]]]):format(id, args, v[2])) -- add to the json table dohicky
+        table.insert(out, string.format("\n\t<Function Name=%q>\n\t\t<Arguments>%q</Arguments>\n\t\t<Return>%q</Return>\n\t\t<Description><![CDATA[DOCUMENT MEH]]></Description>\n\t</Function>",id, args, v[2])) -- add to the json table dohicky
     end
 end
-table.insert(out, "}")
-file.Write("funcs.txt", table.concat(out))
+table.insert(out, "</FunctionList>")
+file.Write("funcs.xml.txt", table.concat(out))
